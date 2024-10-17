8a7fe93bee77ecb5bc5600c7b73d2bc67aed8244
Nadav-Gold <178598444+Nadav-Gold@users.noreply.github.com>
2024-10-16 23:04:05 -0300

    Finished exercise.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,75 @@
-module Enumerable
+module Enumerable # rubocop:disable Style/Documentation
   # Your code goes here
+  def my_map
+    temp = []
+    for element in self # rubocop:disable Style/For
+      temp << yield(element) if block_given?
+    end
+    temp
+  end
+
+  def my_all?
+    return true unless block_given?
+
+    for element in self # rubocop:disable Style/For
+      return false unless yield element
+    end
+    true
+  end
+
+  def my_any?
+    return false if length.zero?
+
+    for element in self # rubocop:disable Style/For
+      return true if yield element
+    end
+    false
+  end
+
+  def my_count
+    return length unless block_given?
+
+    counter = 0
+    for element in self # rubocop:disable Style/For
+      counter += 1 if yield element
+    end
+    counter
+  end
+
+  def my_each_with_index
+    return self unless block_given?
+
+    index = 0
+    for element in self # rubocop:disable Style/For
+      yield element, index
+      index += 1
+    end
+    self
+  end
+
+  def my_inject(initial)
+    for element in self # rubocop:disable Style/For
+      initial = yield initial, element
+    end
+    initial
+  end
+
+  def my_none?
+    return true unless block_given?
+
+    for element in self # rubocop:disable Style/For
+      return false if yield element
+    end
+    true
+  end
+
+  def my_select
+    temp = []
+    for element in self
+      temp << element if yield element
+    end
+    temp
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +78,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for element in self # rubocop:disable Style/For
+      yield element if block_given? # Only yield if a block is provided
+    end
+    self # Return the array itself to allow method chaining
+  end
 end
